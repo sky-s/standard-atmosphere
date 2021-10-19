c602ddf58953f085cb615c0d889024b1a8f8bcfb
Sky Sartorius <sky-s@users.noreply.github.com>
2021-10-19 03:39:32 -0700

    versioned

--- Contents.m
+++ Contents.m
@@ -1,9 +1,10 @@
 % Standard Atmosphere Functions
-% Version 2.2 .
+% Version 2.3 .
 %
 %   atmos       - Gas properties for given altitude in the 1976 Standard Atmosphere.
 %   tropos      - Stripped-down version of atmos, applicable only to the troposphere.
-%   densityalt  - Altitude for given air density.
+%   da          - Altitude in standard atmosphere for a given air density.
+%   densityalt  - Altitude in non-standard atmosphere for given air density.
 %   pressurealt - Approximate altitude in troposphere for given pressure.
 % 
 % https://www.mathworks.com/matlabcentral/fileexchange/28135
